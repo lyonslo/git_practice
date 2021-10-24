45d82ee296d65fa2251f83421bfd2f456acea7db
lyosnlo <lyonslo@163.com>
2021-10-24 14:39:15 +0800

    BUG分支修改

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class Main {
+
+    @Test
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+    }
+    @Test
+    public void testHelloWrold1() {
+        System.out.println("HelloWorld!");
+    }
+}
